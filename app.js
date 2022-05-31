f431525c7cb21f7265af9406c4ae84d4a572d4db
LABARRE Axel <axel.labarre@etud.univ-paris8.fr>
2022-05-31 13:47:32 +0200

    Création de commandes, script commands et server info, new file : deploy-commands.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const { Client, Intents } = require('discord.js');
 const { token } = require('./config.json');
+
 // console.log(token);
 
 /**
@@ -13,6 +14,23 @@ const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MES
 bot.once('ready', () => {
     console.log('Le bot a bien été initialisé!');
 });
+
+bot.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (commandName === 'ping') {
+        await interaction.reply('Pong!');
+    }
+    else if (commandName === 'server') {
+        await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+    }
+    else if (commandName === 'user') {
+        await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+    }
+});
+
 
 bot.on('messageCreate', (msg) => {
     switch (msg.content) {
